0e5d154004ef4bbe2f59bd8ff19673101d3059ee
Beokro <xujiacao@gmail.com>
2017-08-07 00:24:32 -0700

    expand entity class, only need one dict now

--- analyze.py
+++ analyze.py
@@ -1,3 +1,6 @@
+# Todo
+# check if entity already exist before adding it to Dict
+
 filename = "../Lag/LacpAgent.tac"
 file = open(filename, "r")
 
@@ -7,15 +10,27 @@ entityDict = {}
 class entity:
     def __init__( self, name ):
         self.name = name
+        self.inputs = []
+        self.outputs = []
+        self.members = []
         self.inputOf = []
         self.outputOf = []
         self.memberOf = []
+
+    def addInput( self, name ):
+        self.inputs.append( name )
+
+    def addOutput( self, name ):
+        self.outputs.append( name )
+
+    def addMember( self, name ):
+        self.members.append( name )
 
     def isInputOf( self, name ):
         self.inputOf.append( name )
 
     def isOutputOf( self, name ):
-        self.outputOf.append( name )
+        self.OutputOf.append( name )
 
     def isMemberOf( self, name ):
         self.memberOf.append( name )
@@ -25,6 +40,23 @@ def getNameSpace():
     for namespace in currentNameSpace:
         prefix += ( namespace + '::' )
     return prefix
+
+def getEntityName():
+    prefix = ''
+    for namespace in currentNameSpace:
+        prefix += ( namespace + '::' )
+    if len( prefix ) > 2:
+        return prefix[ : -2 ]
+
+def getTypeName( word ):
+    if word[ -1 ] == ';':
+        word = word[ : -1 ]
+
+    if word.endswith( '::Ptr' ):
+        word = word[ : -5 ]
+    elif word.endswith( '::PtrConst' ):
+        word = word[ : -10 ]
+    return word
 
 # return extra line it read
 def skipIntro ( file ):
@@ -74,7 +106,7 @@ def getCompleteLine( file ):
 
 def isNameSpace( words ):
     # assunming all namespace decls have format xxx : Tac::Namespace {
-    return len( words ) == 4 and words[ 2 ] == 'Tac::Namespace' and words[ 3 ] == '{'
+    return len( words ) == 4 and words[ 2] == 'Tac::Namespace' and words[ 3 ] == '{'
 
 def handleNameSpace( words ):
     currentNameSpace.append( words[ 0 ] );
@@ -82,6 +114,9 @@ def handleNameSpace( words ):
 def isForwardDecl( words ):
     return len( words ) == 5 and words[ 2 ] == 'Tac::Type()' and\
         words[ 4 ] == 'Tac::Constrainer;';
+
+def handleForwardDecl( words ):
+    return
 
 def isEntity( words ):
     return len( words ) > 5 and words[ -1 ] == '{' and words[ -2 ] == 'Tac::Entity'
@@ -91,16 +126,33 @@ def handleEntity( words ):
     currentNameSpace.append( words[ 0 ] )
     entityDict[ newEntity.name ] = newEntity
 
+def isElement( words ):
+    return len( words ) == 3 and words[ 1 ] == ':'
+
+def handleElement( words ):
+    # add current element to the entity member list
+    typeName = getTypeName( words[ 2 ] )
+    entityDict[ getEntityName() ].addMember( typeName )
+
+    # check if entity is already in the map
+
 def analyzeFile( file ):
     skipIntro( file )
     words = getCompleteLine( file )
+    typeCheckers = [ isNameSpace,
+                     isForwardDecl,
+                     isEntity,
+                     isElement ]
+    handlers = [ handleNameSpace,
+                 handleForwardDecl,
+                 handleEntity,
+                 handleElement ]
+
     while len( words ):
-        if isNameSpace( words ):
-            handleNameSpace( words )
-        elif isForwardDecl( words ):
-            pass
-        elif isEntity( words ):
-            handleEntity( words )
+        for i in range( len( typeCheckers ) ):
+            if typeCheckers[ i ]( words ):
+                handlers[ i ]( words )
+                break
         words = getCompleteLine( file )
 
 analyzeFile( file )
